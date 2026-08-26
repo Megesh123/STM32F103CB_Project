62d699e48af43b64ef1340ca2a4d8b45d1092946
Megeshwaran <143992106+Megesh123@users.noreply.github.com>
2026-08-26 17:04:20 +0530

    Clarify 7-segment refresh and 1-second update API

--- Core/Inc/seven_seg.h
+++ Core/Inc/seven_seg.h
@@ -13,12 +13,17 @@
  * Digit enable pins:
  *   SEG1 = PA8, SEG2 = PA11, SEG3 = PA12, SEG4 = PA15
  *
- * This implementation assumes a common-cathode display:
+ * Common-cathode display assumption:
  *   segment HIGH = ON, digit enable HIGH = ON.
+ *
+ * TIM2 must call SevenSeg_Tick() every 1 ms.
+ * SevenSeg_Tick() is for continuous multiplex refresh only.
+ * The seconds value is advanced separately once every 1000 ms.
  */
 
 void SevenSeg_Init(void);
 void SevenSeg_SetNumber(uint16_t value);
-void SevenSeg_Tick(void);
+void SevenSeg_Tick(void);       /* Call every 1 ms */
+void SevenSeg_SecondTick(void); /* Call every 1 second */
 
 #endif /* SEVEN_SEG_H */
